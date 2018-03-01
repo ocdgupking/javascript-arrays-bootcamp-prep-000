da65a79652fc7e7454a47b15310142401ea4d86f
guptaco@ymail.com <guptaco@ymail.com>
2018-03-01 03:43:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,6 +28,6 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
 }
 
 function removeElementFromBeginningOfArray(chocolateBars){
-  chocolateBars.slice(0)
+  chocolateBars = chocolateBars.slice(1)
   return chocolateBars
 }

721d4630cfbdffdced732b4b8fa117288b6dab43
guptaco@ymail.com <guptaco@ymail.com>
2018-03-01 03:36:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,6 +18,6 @@ function destructivelyAddElementToEndOfArray(chocolateBars, newCandy){
   return chocolateBars
 }
 
-function accessElementInArray(chocolateBars, [2]){
-  
+function accessElementInArray(chocolateBars, index){
+  return chocolateBars[index]
 }

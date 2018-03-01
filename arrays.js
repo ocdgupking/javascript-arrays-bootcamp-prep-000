01dcb84a40218a2ee23bf1cebf22d36e0cc1a790
guptaco@ymail.com <guptaco@ymail.com>
2018-03-01 02:50:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(chocolateBars, newCandy) {
-  return chocolateBars.unshift(newCandy)
+function addElementToBeginningOfArray(
 }
 
 function destructivelyAddElementToBeginningOfArray() {

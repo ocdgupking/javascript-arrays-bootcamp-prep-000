a53c344c78fd0e0a1e58afa2862c00761c7c80b7
guptaco@ymail.com <guptaco@ymail.com>
2018-03-01 02:52:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(
+function addElementToBeginningOfArray([1], foo){
+  return 
 }
 
 function destructivelyAddElementToBeginningOfArray() {
